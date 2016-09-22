87d943e290c6b7a6c187c0300da58dd1b2bd1a4d
Matt Gaikema <mgaikema1@gmail.com>
2016-09-22 11:48:40 -0500

    begin ts refactoring

---
+++ Bubble.ts
@@ -0,0 +1,14 @@
+export class Bubble
+{
+	public x: number;
+	public y: number;
+
+	private dy: number;
+	private dx: number;
+
+	private radius: number;
+
+	private color;
+
+	private count: number;
+}
---
+++ main.ts
@@ -0,0 +1,13 @@
+// https://github.com/TexAgg/Minesweeper/tree/master/src/scripts
+
+import {Bubble} from "./Bubble";
+
+let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('my_canvas');
+let ctx = canvas.getContext("2d");
+
+document.getElementById('x').setAttribute('max', String(canvas.width));
+document.getElementById('y').setAttribute('max', String(canvas.height));
+
+let ball_radius: number = 30;
+let balls = []; //Storage for all the Bubbles
+let max_balls: number = 30;
